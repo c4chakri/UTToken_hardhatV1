0db395334759c713386dc5ef1b306f173274ad7d
c4chakri <chakravarthi.l@mobiusdtaas.ai>
2024-11-27 11:52:19 +0530

    Create UTToken-v2.sol

---
+++ contracts/UTToken-v2.sol
@@ -0,0 +1,113 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+contract UTToken is ERC20, Ownable, Pausable, ReentrancyGuard {
+    mapping(address => bool) public blackListedAddress;
+
+  
+    uint256 public rewardRate = 10;
+    uint16 public txnTaxRateBasisPoints;
+    address public txnTaxWallet;
+    uint8 private _decimals;
+    IUniswapV2Router02 public uniswapRouter;
+
+    struct smartContractActions {
+        bool canMint;
+        bool canBurn;
+        bool canPause;
+        bool canBlacklist;
+        bool canChangeOwner;
+        bool canTxTax;
+        bool canBuyBack;
+        bool canStake;
+    }
+    struct Stake {
+        uint256 amount;
+        uint256 lockPeriod; 
+        uint256 stakeTimestamp;
+        bool isRewarded; 
+    }
+
+    mapping(address => Stake[]) public userStakes; 
+    mapping(address => uint256) public totalStaked; 
+
+    smartContractActions public actions;
+    event LogApproval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+    event LogTotalSupply(uint256 totalSupply, uint256 decimals);
+
+    modifier canMintModifier() {
+        require(
+            actions.canMint,
+            "Minting Functionality is not enabled in this smart contract!"
+        );
+        _;
+    }
+
+    modifier canBurnModifier() {
+        require(
+            actions.canBurn,
+            "Burning Functionality is not enabled in this smart contract!"
+        );
+        _;
+    }
+
+    modifier canPauseModifier() {
+        require(
+            actions.canPause,
+            "Pause/Unpause Functionality is not enabled in this smart contract!"
+        );
+        _;
+    }
+
+    modifier canBlacklistModifier() {
+        require(
+            actions.canBlacklist,
+            "Blacklist Address Functionality is not enabled in this smart contract!"
+        );
+        _;
+    }
+
+    modifier canChangeOwnerModifier() {
+        require(
+            actions.canChangeOwner,
+            "Change Owner Functionality is not enabled in this smart contract!"
+        );
+        _;
+    }
+
+    modifier canBuyBackModifier() {
+        require(
+            actions.canBuyBack,
+            "Buyback Token Functionality is not enabled in this smart contract!"
+        );
+        _;
+    }
+
+    modifier canStakeModifier() {
+        require(
+            actions.canStake,
+            "Staking reward Functionality is not enabled in this smart contract!"
+        );
+        _;
+    }
+ modifier canTxTaxModifier() {
+        require(
+            actions.canTxTax,
+            "Txn Tax Functionality is not enabled in this smart contract!"
+        );
+        _;
+    }
+    modifier isBlackListed() {
+        require(!blackListedAddress[msg.sender], "User is blacklisted!");
+        _;
+    }
