82fa18175c5a6fe7e56832b721166182d6eba67e
c4chakri <chakravarthi.l@mobiusdtaas.ai>
2025-02-11 16:30:03 +0530

    uups

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('@openzeppelin/hardhat-upgrades');
 
-// const deployerPrivateKey = process.env.DEP_PRIVATE_KEY_1??"0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
